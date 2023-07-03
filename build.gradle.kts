2d917a308cd17fcc23ea91d8c914d46020e39f94
Itz_Hoffe <mhoff770@gmail.com>
2023-07-03 16:44:00 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,23 @@ repositories {
 
 plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("maven-publish")
 }
 
 subprojects {
     plugins.apply("java")
     plugins.apply("maven-publish")
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = project.group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+
+                from(components["java"])
+            }
+        }
+    }
+
 }
