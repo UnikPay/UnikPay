2127480e42180ad2a99cc7a91739ee123c41dcfc
Itz_Hoffe <mhoff770@gmail.com>
2023-08-01 15:24:27 +0200

    update

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -8,3 +8,8 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("com.google.code.gson:gson:2.10.1")
 }
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
