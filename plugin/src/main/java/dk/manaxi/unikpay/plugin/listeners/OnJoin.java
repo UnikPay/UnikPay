47b5d3d4ad555ddf57d78418a58810acdd5d80ee
shad0www <ccff33brj@gmail.com>
2023-08-11 17:48:13 +0200

    Made Join listener ASYNC

--- plugin/src/main/java/dk/manaxi/unikpay/plugin/listeners/OnJoin.java
+++ plugin/src/main/java/dk/manaxi/unikpay/plugin/listeners/OnJoin.java
@@ -4,6 +4,7 @@ import dk.manaxi.unikpay.plugin.Main;
 import dk.manaxi.unikpay.plugin.configuration.Config;
 import dk.manaxi.unikpay.plugin.manager.UpdateManager;
 import dk.manaxi.unikpay.plugin.utils.ColorUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -12,12 +13,13 @@ public class OnJoin implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        if (event.getPlayer().isOp()) {
-            if (UpdateManager.isANewVersionAvailable(Main.getInstance().getDescription().getVersion())) {
+        Bukkit.getScheduler().runTaskAsynchronously(Main.getInstance(), () -> {
+            if (event.getPlayer().isOp() && UpdateManager.isANewVersionAvailable(Main.getInstance().getDescription().getVersion()))  {
                 event.getPlayer().sendMessage(Config.get("prefix")[0]);
                 event.getPlayer().sendMessage(ColorUtils.getColored(" &fDer er en ny version af &aUnikpay.Jar"));
                 event.getPlayer().sendMessage(ColorUtils.getColored(" &fBenyt dig af &a/unikpay update"));
             }
-        }
+        });
+
     }
 }
