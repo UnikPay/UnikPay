f678ff5696fe752b168cf9c4ef27dfb737efe8fb
Itz_Hoffe <mhoff770@gmail.com>
2023-08-15 15:30:44 +0200

    Errors

--- plugin/src/main/java/dk/manaxi/unikpay/plugin/websocket/Console.java
+++ plugin/src/main/java/dk/manaxi/unikpay/plugin/websocket/Console.java
@@ -1,9 +1,13 @@
 package dk.manaxi.unikpay.plugin.websocket;
 
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.appender.AbstractAppender;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.bukkit.Bukkit;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -11,7 +15,7 @@ import java.util.Date;
 public class Console extends AbstractAppender {
     SimpleDateFormat formatter;
     public Console() {
-        super("Log4JAppender", null, null, false, null);
+        super("Log4JAppender", null, null);
         formatter = new SimpleDateFormat("HH:mm:ss");
     }
     @Override
@@ -21,9 +25,18 @@ public class Console extends AbstractAppender {
     @Override
     public void append(LogEvent event) {
         String message = event.getMessage().getFormattedMessage();
-        message = "[" +formatter.format(new Date()) + " " + event.getLevel().toString() + "] " + message;
-        if(IoSocket.getSocket() != null && IoSocket.getSocket().connected()) {
-            IoSocket.getSocket().emit("console", message);
+        String formattedMessage = "[" + formatter.format(new Date()) + " " + event.getLevel().toString() + "] " + message;
+        Throwable throwable = event.getThrown();
+        if (throwable != null) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            throwable.printStackTrace(pw);
+            String exceptionDetails = sw.toString();
+            formattedMessage += "\n" + exceptionDetails;
+        }
+
+        if (IoSocket.getSocket() != null && IoSocket.getSocket().connected()) {
+            IoSocket.getSocket().emit("console", formattedMessage);
         }
     }
 }
