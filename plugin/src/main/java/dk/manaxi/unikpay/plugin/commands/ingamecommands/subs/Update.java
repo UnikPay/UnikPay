140058f275e567d1614ee6c41087dd8963b55c43
Itz_Hoffe <mhoff770@gmail.com>
2024-02-14 03:21:10 +0100

    update command also

--- plugin/src/main/java/dk/manaxi/unikpay/plugin/commands/ingamecommands/subs/Update.java
+++ plugin/src/main/java/dk/manaxi/unikpay/plugin/commands/ingamecommands/subs/Update.java
@@ -5,7 +5,9 @@ import dk.manaxi.unikpay.plugin.commands.ISubCommand;
 import dk.manaxi.unikpay.plugin.configuration.Config;
 import dk.manaxi.unikpay.plugin.manager.UpdateManager;
 import dk.manaxi.unikpay.plugin.utils.ColorUtils;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.io.IOException;
 
@@ -16,20 +18,22 @@ public class Update extends ISubCommand {
 
     @Override
     public void onCommand(CommandSender sender, String[] args, String paramString) {
-        if (!sender.hasPermission(Main.configYML.getString("admin-permission")) && !sender.hasPermission("unikpay.reload")) {
-            Config.send(sender, "no-permission");
+        if(!(sender instanceof Player)) return;
+        Player player = (Player) sender;
+        if (!player.hasPermission(Main.configYML.getString("admin-permission")) && !player.hasPermission("unikpay.reload")) {
+            Main.getInstance().getLang().send(player, "no-permission");
             return;
         }
-        sender.sendMessage(Config.get("prefix")[0]);
-        sender.sendMessage(ColorUtils.getColored(" &fBegynder at opdatere pluginet"));
+        Main.getInstance().getLang().send(player, "unikpay.update.start",
+                Placeholder.component("prefix", Main.getInstance().getLang().get("prefix")));
         try {
             UpdateManager.Update();
-            sender.sendMessage(ColorUtils.getColored(" &aDu downloaded og installerede den nyeste version af pluginet."));
-            sender.sendMessage(ColorUtils.getColored(" &aFor at få den nye version på skal du genstart din server."));
+            Main.getInstance().getLang().send(player, "unikpay.update.success",
+                    Placeholder.component("prefix", Main.getInstance().getLang().get("prefix")));
         } catch (IOException e) {
-            sender.sendMessage(ColorUtils.getColored(" &cDer skete en fejl, under opdatering. &7&o( &fTjek logs &7&o)"));
-            throw new RuntimeException(e);
-
+            Main.getInstance().getLang().send(player, "unikpay.update.error",
+                    Placeholder.component("prefix", Main.getInstance().getLang().get("prefix")));
+            e.printStackTrace();
         }
     }
 }
