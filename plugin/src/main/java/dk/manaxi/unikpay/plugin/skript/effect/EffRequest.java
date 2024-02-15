645f0af9ef8bb526553912dc348c3ab62a91e0db
Itz_Hoffe <mhoff770@gmail.com>
2024-02-15 14:43:31 +0100

    Rename

--- plugin/src/main/java/dk/manaxi/unikpay/plugin/skript/effect/EffRequest.java
+++ plugin/src/main/java/dk/manaxi/unikpay/plugin/skript/effect/EffRequest.java
@@ -5,8 +5,6 @@ import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.util.Kleenean;
-import dk.manaxi.unikpay.api.Config;
-import dk.manaxi.unikpay.api.classes.Pakke;
 import dk.manaxi.unikpay.plugin.API.Internal;
 import dk.manaxi.unikpay.plugin.Main;
 import org.bukkit.entity.Player;
@@ -14,38 +12,47 @@ import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class EffAnmodPakker extends Effect {
-    private static final String url = Main.getInstance().getConfigSystem().getUrl() + "/v1/request";
-
+public class EffRequest extends Effect {
     private Expression<Player> player;
-    private Expression<Pakke> pakke;
+    private Expression<Number> amount;
+    private Expression<String> pakke;
+    private Expression<String> id;
 
 
     static {
-        Skript.registerEffect(EffAnmodPakker.class, "[unikpay] (request|anmod) %player% (pakker|packages) %pakkes%");
+        Skript.registerEffect(EffRequest.class, "[unikpay] (request|anmod) %player% [om ]%number% em[(eralds|s|eralder|erald)] for %string%[ (med id|with id) %-string%]");
     }
 
     @Override
     protected void execute(@NotNull Event event) {
         final Player player = this.player.getSingle(event);
-        final Pakke[] pakker = this.pakke.getAll(event);
+        final Number amount = this.amount.getSingle(event);
+        final String pakke = this.pakke.getSingle(event);
+        final String id = this.id != null ? this.id.getSingle(event) : pakke;
+
+        if (Main.getAPIKEY() == null) {
+            Skript.error("Du mangler at putte din apikey ind i config.yml");
+            return;
+        }
 
         if (player == null || pakke == null)
             return;
 
-        Internal.sendPackageRequest(player, pakker);
+        Internal.sendPackageRequest(player, pakke, amount, id);
     }
 
     @Override
     public @NotNull String toString(@Nullable Event e, boolean debug) {
-        return "[unikpay] (request|anmod) %player% (pakker|packages) %pakkes%";
+        return "[unikpay] (request|anmod) %player% [om ]%number% em(eralds|s|eralder) for %string%[ (med id|with id) %-string%]";
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public boolean init(Expression<?>[] expressions, int matchedPattern, @NotNull Kleenean isDelayed, @NotNull SkriptParser.ParseResult parseResult) {
         this.player = (Expression<Player>) expressions[0];
-        this.pakke = (Expression<Pakke>) expressions[1];
+        this.amount = (Expression<Number>) expressions[1];
+        this.pakke = (Expression<String>) expressions[2];
+        this.id = (Expression<String>) expressions[3];
         return true;
     }
 }
